bad70e7868bf5b8809a3b6374d88d9c74ad20d0e
Elisabeth de Vries <elisabeth.kloren@crow.nl>
2023-07-16 11:20:15 +0200

    initiate processeniampro

---
+++ docs/processeniampro/config.js
@@ -0,0 +1,16 @@
+var respecConfig = {
+  // => https://github.com/stichting-crow/respec/wiki
+  specStatus: "DRAFT",
+  shortName: "procesmodel/processeniampro",
+  specType: "Algemeen", // vrije tekst, kleine letter
+  imprint: "crow",
+  editors: [
+    // alleen `name` is verplicht, https://respec.org/docs/#person
+    {
+      name: "Elisabeth de Vries",
+    },
+  ],
+  github: "https://github.com/Stichting-CROW/procesmodel",
+  license: "cc-by",
+  lint: { "no-unused-dfns": false },
+};
---
+++ docs/processeniampro/h/0-samenvatting.md
@@ -0,0 +1,2 @@
+Dit document geeft een overzicht van werkprocessen bij elke processtap in het iAMPro model, en bijbehorende standaarden voor elk werkproces.
+
